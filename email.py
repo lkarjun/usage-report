7300c4962badabb9c23dd985b3be63c24c08a07e
lkarjun <lk.arjun@hotmail.com>
2020-06-25 22:37:38 +0530

    authentication fixed

---
+++ email.py
@@ -0,0 +1,20 @@
+import smtplib
+
+
+def load_credential():
+    """ to load some basic credential """
+    pass
+
+
+def main(message: str) -> str:
+    """ for sending report """
+    try:
+        mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+        send_address = "techminded25@gmail.com"
+        passs = "Iphone@670"
+        to_address = load_credential()
+        mail_server.login(send_address, passs)
+        mail_server.sendmail(send_address, to_address, message)
+        
+    except Exception as e:
+        return f'Please contact Technical team issue is: {e}'
