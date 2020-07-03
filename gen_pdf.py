fd83bf04bd286ae6ad8a5039629e89cf01c9d9a4
lkarjun <lk.arjun@hotmail.com>
2020-07-03 12:27:01 +0530

    Outline Fixed

--- gen_pdf.py
+++ gen_pdf.py
@@ -1,14 +1,42 @@
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
-from reportlab.lib.styles import getSampleStyleSheet
+import datetime 
+import time
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
 
+fileName = 'tem/report.pdf'
 
-def genPdf() -> int:
-    '''
-    generates pdf
-    '''
-    report = SimpleDocTemplate("tem/report.pdf")
-    styles =  getSampleStyleSheet()
-    report_title = Paragraph("Daily Usage Report", styles["h1"])
-    report.build([report_title])
+doc = SimpleDocTemplate(fileName,pagesize=letter,
+                        rightMargin=72,leftMargin=72,
+                        topMargin=72,bottomMargin=18)
 
+Story=[]
+
+mainTitle = 'APP USAGE REPORT'
+d = datetime.date.today()
+reportImage = 'fig.png'
+subtitle = 'Daily Report'
+formatted_time = time.ctime()
+im = Image(reportImage,2*inch,2*inch)
+
+styles=getSampleStyleSheet()
+
+#Main Heading
+styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+ptext = '<font size="25">%s</font>' % mainTitle
+Story.append(Paragraph(ptext, styles["Normal"]))
+Story.append(Spacer(2,40))
+
+#sub heading
+ptext = '<font size="18">%s</font>' % subtitle
+Story.append(Paragraph(ptext, styles["Normal"]))
+Story.append(Spacer(1, 20))
+
+#Generating Time
+ptext = '<font size="10">%s</font>' % formatted_time
+Story.append(Paragraph(ptext, styles["Normal"]))
+Story.append(Spacer(1, 20))
+
+doc.build(Story)
