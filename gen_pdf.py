845d474dd8b0dc771ac09d978cdc0405c7294026
lkarjun <lk.arjun@hotmail.com>
2020-07-04 12:23:11 +0530

    Fixed image part

--- gen_pdf.py
+++ gen_pdf.py
@@ -13,30 +13,38 @@ doc = SimpleDocTemplate(fileName,pagesize=letter,
                         topMargin=72,bottomMargin=18)
 
 Story=[]
+def main() -> int:
+    mainTitle = 'APP USAGE REPORT'
+    d = datetime.date.today()
+    reportImage = 'tem/figure.png'
+    subtitle = 'Daily Report'
+    formatted_time = time.ctime()
+    im = Image(reportImage,5.5*inch,4*inch)
 
-mainTitle = 'APP USAGE REPORT'
-d = datetime.date.today()
-reportImage = 'fig.png'
-subtitle = 'Daily Report'
-formatted_time = time.ctime()
-im = Image(reportImage,2*inch,2*inch)
+    styles=getSampleStyleSheet()
 
-styles=getSampleStyleSheet()
+    #Main Heading
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+    ptext = '<font size="25">%s</font>' % mainTitle
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(2,40))
 
-#Main Heading
-styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-ptext = '<font size="25">%s</font>' % mainTitle
-Story.append(Paragraph(ptext, styles["Normal"]))
-Story.append(Spacer(2,40))
+    #sub heading
+    ptext = '<font size="18">%s</font>' % subtitle
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 20))
 
-#sub heading
-ptext = '<font size="18">%s</font>' % subtitle
-Story.append(Paragraph(ptext, styles["Normal"]))
-Story.append(Spacer(1, 20))
+    #Generating Time
+    ptext = '<font size="10">%s</font>' % formatted_time
+    Story.append(Paragraph(ptext, styles["Normal"]))
+    Story.append(Spacer(1, 20))
 
-#Generating Time
-ptext = '<font size="10">%s</font>' % formatted_time
-Story.append(Paragraph(ptext, styles["Normal"]))
-Story.append(Spacer(1, 20))
+    #Matplotlib image display here
+    Story.append(im)
+    Story.append(Spacer(2,45))
 
-doc.build(Story)
+    ptext = '<font size="12">Made with ❤ by LKA</font>'
+    Story.append(Paragraph(ptext, styles["Justify"]))
+    doc.build(Story)
+
+    return 0
