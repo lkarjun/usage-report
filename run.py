b7b5ea60decf345944b42c657bc9522314fcf6c6
lkarjun <lk.arjun@hotmail.com>
2020-07-09 15:01:54 +0530

    listprocessor() Finished

--- run.py
+++ run.py
@@ -2,15 +2,31 @@ import re
 import winapps
 from winapps import list_installed
 import psutil
+from typing import List, Literal
 
-apps_load = winapps.list_installed()
-#listing and converting to string
-listing_all_apps = [str(i) for i in apps_load]
-regrex = r"name=['][\w\s]*[']"
-apps = [
-        app[6:-1]
-        for detail in listing_all_apps
-        for app in re.findall(regrex, detail)
+def listapps() -> List:
+        apps_load = winapps.list_installed()
+        #listing and converting to string
+        listing_all_apps = [str(i) for i in apps_load]
+        regrex = r"name=['][\w\s]*[']"
+        apps = [
+                app[6:-1]
+                for detail in listing_all_apps
+                for app in re.findall(regrex, detail)
+                ]
+        return list(set(apps))
+
+def listprocessor() -> List:
+        apps_load = psutil.process_iter()
+        listing_all_apps = [str(i) for i in apps_load]
+        regrex = r"name=['][\w\s.]*[']"
+        apps = [
+                app[6:-5]
+                for detail in listing_all_apps
+                for app in re.findall(regrex, detail)
         ]
+        return list(set(apps))
 
-print(apps)
+if __name__ == "__main__":
+        pass
+        
