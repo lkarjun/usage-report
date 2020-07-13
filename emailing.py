2f1dfbad66d03ac30b95ef67b053690e193ab35b
lkarjun <lk.arjun@hotmail.com>
2020-07-13 11:25:23 +0530

    File Renamed

--- emailing.py
+++ emailing.py
@@ -1,9 +1,12 @@
+import re
 import smtplib
 from authenticate import FileManage
+import email.message
+import mimetypes
 
 auth = FileManage()
 
-def main(message: str) -> int:
+def send_email(message: str) -> int:
     """ for sending report """
     try:
         mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
@@ -13,6 +16,16 @@ def main(message: str) -> int:
         to_address = auth.sendReceiverEmail()
         mail_server.login(send_address, passs)
         mail_server.sendmail(send_address, to_address, message)
+        mail_server.quit()
         return 0
     except:
         return 1
+
+def generate_error_report(subject: str):
+    message = email.message.EmailMessage()
+    message["From"] = auth.sendSenderEmail()
+    message["To"] = auth.sendReceiverEmail()
+    message["Subject"] = subject
+    body = "Kindly Please check Your system, and resolve it."
+    message.set_content(body)
+    return message
